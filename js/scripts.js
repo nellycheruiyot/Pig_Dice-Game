76a324fbbf3dc7dbccd0b414d527548ed8702f63
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-08-25 17:13:10 +0300

    Add content to player section of the game

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,53 @@
+function Player(playerName, score, turnScore ) {
+  this.playerName = playerName;
+  this.turnScore = score;
+  this.turnTotal = turnScore;
+}
 
+Player.prototype.rollDie = function() {
+  var rollScore = 0
+  var roll = Math.floor(Math.random()*6+1);
+  if (roll !===1) {
+    this.turnScore += rollScore;
+  }else {
+    rollScore = 0;
+    this.turnScore = 0;
+    alert("Your turn is up!")
+  }
+}
+
+Player.prototype.hold = function() {
+  this.score += this.turnScore
+  this.turnScore = 0;
+}
+
+Player.prototype.anotherTurn = function() {
+  this.turnScore = 0;
+}
+
+Player.prototype.win = function() {
+  if(this.score >= 100) {
+    alert("You Win!")
+  }
+}
+
+Player.prototype.newGame = function() {
+  this.turnScore = 0;
+  this.score = 0;
+}
+
+$(document).ready(function(){
+  $(".row-start").hide();
+
+  $("#game").submit(function(event) {
+    event.preventDefault();
+
+    var player1input = $("#player1").val();
+    var plaey2input = $("#player2").val();
+
+    $(".player1").text(player1input);
+    $(".player2").text(player2input);
+
+
+  })
+});
