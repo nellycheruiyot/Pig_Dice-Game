ae79c61e256723ce49f47b247a24d50ee75fb587
Nelly Cheruiyot <nellycheruiyot@gmail.com>
2017-08-27 01:27:35 +0300

    Modify to include business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 //business logic
-function Player(playerName, score, turnScore ) {
+function Player(playerName) {
   this.playerName = playerName;
-  this.turnScore = score;
-  this.turnTotal = turnScore;
+  this.rollScore = 0;
+  this.turnScore = 0;
+  this.totalScore = 0;
 }
 
 Player.prototype.rollDie = function() {
   var roll = Math.floor((Math.random()*6)+1);
-  console.log(roll);
+  //console.log(roll);
+  this.rollScore = roll;
   if (roll !==1) {
     this.turnScore += roll;
   }else {
@@ -18,7 +20,7 @@ Player.prototype.rollDie = function() {
 }
 
 Player.prototype.hold = function() {
-  this.score += this.turnScore
+  this.totalScore += this.turnScore
   this.turnScore = 0;
 }
 
@@ -36,7 +38,7 @@ Player.prototype.newGame = function() {
   this.turnScore = 0;
   this.score = 0;
 }
-var Player1, Player2;
+var player_1, player_2, player1Input, player2Input;
 //user interface
 $(document).ready(function(){
   $(".container-play").hide();
@@ -44,18 +46,92 @@ $(document).ready(function(){
   $("form#game").submit(function(event) {
     event.preventDefault();
 
-    var inputtedPlayer1 = $("input#player1").val();
-    var inputtedPlayer2 = $("input#player2").val();
+    player1Input = $("input#player1").val();
+    player2Input = $("input#player2").val();
 
-    $(".player1").text(inputtedPlayer1);
-    $(".player2").text(inputtedPlayer2);
+    $(".player1").text(player1Input);
+    $(".player2").text(player2Input);
 
-
-    Player1 = new Player(inputtedPlayer1, 0, 0)
+    $(".row-start").hide();
     $(".container-play").show();
+
+
+    player_1 = new Player(player1Input);
+    player_2 = new Player(player2Input);
   })
+
   $("#roll-button1").click(function(event) {
     event.preventDefault
-    var rollPlayer1 = Player1.rollDie();
+    player_1.rollDie();
+    $("#score1").text(player_1.rollScore);
+    $(".turnScore1").text(player_1.turnScore);
+    $(".totalScore1").text(player_1.totalScore);
   });
+
+  $("#hold-button1").click(function(event) {
+    event.preventDefault
+    player_1.hold();
+    $(".totalScore1").text(player_1.totalScore);
+    if (player_1.totalScore >= 10) {
+      alert(player_1.playerName + " won! Please start a new game.");
+      $(".winner").text(player_1.playerName);
+      $("#roll-button1").hide();
+      $("#hold-button1").hide();
+      $("#roll-button2").hide();
+      $("#hold-button2").hide();
+    }else {
+      alert("Your turn is up! Please pass the mouse to " + player_2.playerName);
+      $("#roll-button1").hide();
+      $("#hold-button1").hide();
+      $("#roll-button2").show();
+      $("#hold-button2").show();
+    }
+  });
+
+  $("#roll-button2").click(function(event) {
+    event.preventDefault
+    player_2.rollDie();
+    $("#score2").text(player_2.rollScore);
+    $(".turnScore2").text(player_2.turnScore);
+    $(".totalScore2").text(player_2.totalScore);
+  });
+
+  $("#hold-button2").click(function(event) {
+    event.preventDefault
+    player_2.hold();
+    $(".totalScore2").text(player_2.totalScore);
+    if (player_2.totalScore >= 10) {
+      alert(player_2.playerName + " won! Please start a new game.");
+      $(".winner").text(player_2.playerName);
+      $("#roll-button1").hide();
+      $("#hold-button1").hide();
+      $("#roll-button2").hide();
+      $("#hold-button2").hide();
+    }else {
+      alert("Your turn is up! Please pass the mouse to " + player_1.playerName);
+      $("#roll-button1").show();
+      $("#hold-button1").show();
+      $("#roll-button2").hide();
+      $("#hold-button2").hide();
+    }
+  });
+
+
+    $("#new-game").click(function(event) {
+      event.preventDefault
+      player_1.playerName="";
+      player_1.rollScore=0;
+      player_1.turnScore=0;
+      player_1.totalScore=0;
+      player_2.playerName="";
+      player_2.rollScore=0;
+      player_2.turnScore=0;
+      player_2.totalScore=0;
+      $(".container-play").hide();
+      $(".row-start").show();
+
+    });
+
+
+
 });
